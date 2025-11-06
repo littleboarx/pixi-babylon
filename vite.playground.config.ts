0280d60cbdf7c223850fb90433f45b0a445dbe45
littleboarx <mrdmanxx@gmail.com>
2025-11-06 17:41:08 +0800

    init docs

--- vite.playground.config.ts
+++ vite.playground.config.ts
@@ -6,9 +6,19 @@ export default defineConfig({
     root: 'playground',
     base: './',
     build: {
+        // lib: {
+        //     formats: ['iife'],
+        // },
+        minify: true,
         outDir: '../docs',
         emptyOutDir: true,
-        sourcemap: true,
+        // rollupOptions: {
+        //     input: './playground/index.ts', // 用 JS 入口代替 HTML
+        //     output: {
+        //         format: 'iife', // 强制 IIFE
+        //         name: 'MyApp', // 全局变量名
+        //     },
+        // },
     },
     resolve: {
         alias: {
